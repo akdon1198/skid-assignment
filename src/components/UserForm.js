009e32a3f37fa1eb5ddbcd832cd512e41560fe96
akdon1198 <akdon1198@gmail.com>
2023-06-21 09:04:30 +0530

    commited

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { useDispatch, useSelector} from "react-redux";
+import { useDispatch} from "react-redux";
 import { adduser, updateuser } from "../redux/UserRedux";
 import {useHistory} from "react-router-dom"
 function UserForm({id}){
